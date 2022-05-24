f64864431658f019a6905a2135c4dcd58bc75e6f
Lee wall <lmw17@my.fsu.edu>
2022-05-23 22:34:38 -0400

    fixed a syntax issue in modularizing the model

--- model.py
+++ model.py
@@ -2,18 +2,44 @@ import numpy as np
 from scipy.integrate import odeint  
 import matplotlib.pyplot as plt 
 
-# how do we solve the global variable problem ? 
-# how do we solve the parameter problem? 
 class Model:
-	# hmm
-	# variable arguments based on model type? or just use string to specify parameters...
-	def initialize_patch_model(n, frac_nomove):
+	
+	# variable arguments based on model type? 
+	# need a function to tinker with individual parameters, and a function to list them 
+	def __init__(self, model_type, n, frac_nomove): 
+
+		# load_parameters() -- hypothetical function to load all params of RB model into this object  
+		if model_type == 'RB':
+			self.load_parameters('RB')
+			# self.RB_initialize_patch_model(n, frac_nomove)
+
+		elif model_type == 'BM':
+			# self.initialize_patch_model(n, frac_nomove)
+			self.load_parameters('BM')
+		elif model_type == 'vdL_PC':
+			self.load_parameters('vdL_PC')
+			# self.initialize_patch_model(n, frac_nomove)
+		elif model_type == 'vdL_MP':
+			self.load_parameters('vdL_MP')
+			# self.initialize_patch_model(n, frac_nomove)
+		elif model_type == 'vdL_MC':
+			# self.initialize_patch_model(n, frac_nomove)
+			self.load_parameters('vdL_MC')
+		elif model_type == 'vdL': # all feedbacks active 
+			self.initialize_patch_model(n, frac_nomove)
+		else:
+			print("Bad input, defaulting to Blackwood-Mumby!")
+			self.initialize_patch_model(n, frac_nomove)
+			# self.load_parameters('BM')
+
+	
+	def initialize_patch_model(self, n, frac_nomove):
 		# do variables defined in this function only exist in the scope of this function if it is called by constructor?
 
 		frac_dispersed = (1-frac_nomove)*(1/(n)) # fraction of fish that disperse to other patches symmetrically
 
 		# transition matrix for dispersal: element [i,j] of kP describes influx of P from j to i
-		kP = np.empty((n,n))
+		kP = np.empty((n,n)) 
 		for i in range(n):
 			for j in range(n):
 				kP[i][j] = frac_dispersed
@@ -31,67 +57,9 @@ class Model:
 		dMs = np.empty(n)
 
 		#concatenate initial condition arrays 
-		
-		X1 = [P0]*n + [C0L]*n + [M0H]*n
-		X2 = [P0]*n + [C0H]*n + [M0L]*n
-
-	def __init__(self, model_type, n, frac_nomove): 
-
-		# instead of global variables, we can initialize the arrays here
-		# so that we could just do self.P[i] for the ith element of the parrotfish array 
-		# how can we just load the parameters conditionally from another file 
-
-		if model_type == 'RB':
-			# load_parameters() -- hypothetical function to load all params of RB model into this object  
-			load_parameters('RB')
-			# RB_initialize_patch_model(n, frac_nomove)
-
-		elif model_type == 'BM':
-			# initialize_patch_model(n, frac_nomove)
-			load_parameters('BM')
-		elif model_type == 'vdL_PC':
-			load_parameters('vdL_PC')
-			# initialize_patch_model(n, frac_nomove)
-		elif model_type == 'vdL_MP':
-			load_parameters('vdL_MP')
-			# initialize_patch_model(n, frac_nomove)
-		elif model_type == 'vdL_MC':
-			# initialize_patch_model(n, frac_nomove)
-			load_parameters('vdL_MC')
-		elif model_type == 'vdL': # all feedbacks active 
-			frac_dispersed = (1-frac_nomove)*(1/(n)) # fraction of fish that disperse to other patches symmetrically
-
-			# transition matrix for dispersal: element [i,j] of kP describes influx of P from j to i
-			kP = np.empty((n,n))
-			for i in range(n):
-				for j in range(n):
-					kP[i][j] = frac_dispersed
-					if i == j:
-						kP[i][j] = -frac_dispersed*(n - 1)
-
-			# instead of global variables here, can we initialize these within patch system? or pass them as parameters? 
-
-			P_influx = np.empty(n)
-			P = np.empty(n) 
-			C = np.empty(n) 
-			M = np.empty(n)
-			dPs = np.empty(n)
-			dCs = np.empty(n)
-			dMs = np.empty(n)
-
-			#concatenate initial condition arrays -- we can put this somewhere else 
-			
-			X1 = [P0]*n + [C0L]*n + [M0H]*n
-			X2 = [P0]*n + [C0H]*n + [M0L]*n
-
-			# initialize_patch_model(n, frac_nomove)
-			# load_parameters('vdL')
-		else:
-			print("Bad input, defaulting to Blackwood-Mumby!")
-			# initialize_patch_model(n, frac_nomove)
-			# load_parameters('BM')
-
-	
+		# need to define baseline values somewhere 
+		# X1 = [P0]*n + [C0L]*n + [M0H]*n
+		# X2 = [P0]*n + [C0H]*n + [M0L]*n
 
 	# rass briggs model is only one with different state variables, so just have a separate init function 
 	# better way might be to give initialize... an optional argument 
@@ -300,6 +268,7 @@ class Model:
 		# check input
 		return None 
 
+	def load_parameters(self, model_type):
+		return None 
 
-
-x = Model('vDL', 2, 1)
+x = Model('vdL', 2, 1)
