7e1817baba322ba5b22f874eb0063597cf7dac3e
Lee wall <lmw17@my.fsu.edu>
2022-05-25 21:16:10 -0400

    finally got something running!

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint  
 import matplotlib.pyplot as plt 
+import math
 
 class Model:
 	
@@ -10,6 +11,12 @@ class Model:
 		self.model_type = model_type
 		self.n = n 
 		self.frac_nomove = frac_nomove
+
+		self.f = 0
+		self.closure_length = 0
+		self.m = 0 
+		self.poaching = 0
+
 
 		'''
 		#time -- maybe move this down to run model for dynamic stuff, same with ICs
@@ -21,7 +28,8 @@ class Model:
 
 		initialize_patch_model()
 		'''
-	def initialize_patch_model(self):
+
+	def initialize_patch_model(self, P0, C0L, C0H, M0L, M0H):
 		# do variables defined in this function only exist in the scope of this function if it is called by constructor?
 
 		frac_dispersed = (1-self.frac_nomove)*(1/(self.n)) # fraction of fish that disperse to other patches symmetrically
@@ -43,11 +51,12 @@ class Model:
 		setattr(self,'dPs', np.empty(self.n))
 		setattr(self,'dCs', np.empty(self.n))
 		setattr(self,'dMs', np.empty(self.n))
-
-		#concatenate initial condition arrays 
-		# need to define baseline values somewhere 
-		# X1 = [P0]*n + [C0L]*n + [M0H]*n
-		# X2 = [P0]*n + [C0H]*n + [M0L]*n
+		setattr(self, 'state_vec', [self.P,self.C,self.M])
+		# concatenate initial condition arrays 
+		# need to define baseline values somewhere
+		# should these be attributes ?  
+		setattr(self, 'X1', [P0]*self.n + [C0L]*self.n + [M0H]*self.n)
+		setattr(self, 'X2', [P0]*self.n + [C0H]*self.n + [M0L]*self.n)
 
 	# rass briggs model is only one with different state variables, so just have a separate init function 
 	# better way might be to give initialize... an optional argument 
@@ -72,8 +81,7 @@ class Model:
 		setattr(self,'dCs', np.empty(self.n))
 		setattr(self,'dMis', np.empty(self.n))
 		setattr(self,'dMvs', np.empty(self.n))
-
-
+		setattr(self, 'state_vec', [P,C,Mv,Mi])
 		# instead of global variables, can we pass these directly to patch_system? 
 		# concatenate initial condition arrays -- amend to match state vec
 		# X1 = [P0]*n + [C0L]*n + [M0H]*n 
@@ -83,7 +91,9 @@ class Model:
 	# this defines the system conditionally based on user input -- this is the function we pass to odeint method
 	# will class formatting mess up odeint?  
 	# maybe put this outside class with Model as an arg? 
-	def patch_system(self, X, t):
+
+	'''
+	def patch_system(self):
 
 		P_influx = [0]*n
 
@@ -94,30 +104,38 @@ class Model:
 			# better way to do this ?
 
 			if self.model_type == 'RB':
+				X = [self.P, self.C, self.Mv, Mi]
 				return rass_briggs(X, i)
 			elif self.model_type == 'BM':
+				X = [self.P, self.C, self.M]
 				return blackwood(X, i)
-
 			elif self.model_type == 'vdL_PC':
+				X = [self.P, self.C, self.M]
 				return leemput(X, i)
 
 			elif self.model_type == 'vdL_MP':
+				X = [self.P, self.C, self.M]
 				return leemput(X, i)
 				
 			elif self.model_type == 'vdL_MC':
+				X = [self.P, self.C, self.M]
 				return leemput(X, i)
 				
 			elif self.model_type == 'vdL': # all feedbacks active 
+				X = [self.P, self.C, self.M]
 				return leemput(X, i)
 				
 			else:
 				print("Bad input, defaulting to Blackwood-Mumby!")
+				X = [self.P, self.C, self.M]
 				return blackwood(X, i)
+	'''
 
 	# returns the model run for a certain set of parameters 
 	# maybe create a subclass for a model run to separate plotting jobs ? 
-	def run_model(IC_set, t, closure_length, f, m, n, poaching):
-		sol = odeint(patch_system, self.ICs, self.t, args = (closure_length, f/(1-m/self.n), m, self.n, self.poaching))
+	def run_model(self, IC_set, t):
+		# can we not pass self as an argument here ? 
+		sol = odeint(patch_system, IC_set, t, args = (self, ))
 		return sol 
 
 	# make setting for show versus save (make these optional arguments) 
@@ -185,71 +203,7 @@ class Model:
 		return None 
 
 	
-	def K(sigma, C):
-		return (1-sigma)+sigma*C
 
-	def square_signal(t, closure_length, region, m, n, poaching):
-		start = int((t % (n*closure_length))/closure_length)
-		if start+m-1 >= n:
-			end = (start + m - 1)%n
-
-		else:
-			end = (start + m - 1)
-		if region >= start and region <= end:
-			return poaching
-		elif start + m - 1 >= n and (region >= start or region <= end):
-			return poaching
-		else:
-			#determine whether to bake displacement into signal
-			return (1-(m/n)*poaching)#/(1-(m/n))
-		
-	#this signal function is not quite working yet 
-	def sigmoid_signal(t, period, p):
-		if period == 0:
-			return 0
-		else:
-			return 1.0 / (1 + math.exp(-(t % period - p * period)))
-
-	def fishing(parrotfish, f):
-		return f/(1+math.exp(-steepness*(parrotfish-shift)))
-
-	# is self keyword necessary here ? or can we call these functions regardless?
-	# is it necessary to send X as a parameter if it is embedded in the class? 
-	# should all of these be class methods which can change self.X? 
-	def blackwood(self, X, i):
-		P, C, M = X.reshape(3, n)
-
-		dPs[i] = s*P[i]*(1 - (P[i] / (beta*self.K(C[i])))) - self.fishing(P[i], f)*P[i]*self.square_signal(t, closure_length, i, m, n, poaching)
-		dCs[i] = r*(1-M[i]-C[i])*C[i]-d*C[i] - a*M[i]*C[i] + i_C*(1-M[i]-C[i])
-		# need to define g(P)
-		dMs[i] = a*M[i]*C[i] - g(P[i])*M[i] *(1/(1-C[i])) + gamma*M[i]*(1-M[i]-C[i])+i_M*(1-M[i]-C[i])
-
-		return np.concatenate((dPs, dCs, dMs), axis=0)
-
-	def leemput(self, X, i):
-		# check input 
-		P,C,M = X.reshape(3, n) # will reshaping work since we are passing arrays of length n? 
-		dPs[i] = P_influx[i]+ s*P[i]*(1 - (P[i] / self.K(sigma,C[i]))) - self.fishing(P[i], f)*P[i] *(self.square_signal(t, closure_length, i, m, n, poaching))
-		dCs[i] = (i_C + r*C[i])*(1-M[i]-C[i])*(1-alpha*M[i]) - d*C[i]
-		
-		dMs[i] = (i_M+gamma*M[i])*(1-M[i]-C[i])-g*M[i]*P[i]/(g*eta*M[i]+1)
-
-		#concatenate into 1D vector to pass to next step
-		return np.concatenate((dPs, dCs, dMs), axis=0)
-
-	def rass_briggs(self, X, i):
-
-		P, C, Mv, Mi = X.reshape(4, n)
-		T = 1 - C - Mv - Mi 
-		dPdt = rH*P*(1-P/K) - self.fishing(P[i], f)*P[i] *(self.square_signal(t, closure_length, i, m, n, poaching))
-		dCdt = (phiC*T) + gTC*T*C - gamma*gTI*Mi*C - dC*C
-		dMvdt = phiM*T + rM*T*Mi + gTV*T*Mv - dV*Mv - P*Mv*Graze - omega * Mv
-		# conceptual question: why is that second term multiplied by M not Mv? 
-		dMidt = omega*Mv + gTI*T*Mi + gamma*gTI*Mi*C - dI*Mi
-		return [dPdt, dCdt, dMvdt, dMidt]
-
-		# check input
-		return None 
 
 	# modify this to take custom feedback parameters, or maybe custom anything? 
 	# this doesn't work for some reason
@@ -267,7 +221,12 @@ class Model:
 			"s" : 1,
 			"sigma" : .5, #strength of coral-herbivore feedback
 			"eta" : 2, #strength of algae-herbivore feedback
-			"alpha" : 0.5 #strength of algae-coral feedback 
+			"alpha" : 0.5, #strength of algae-coral feedback 
+			"P0" : 0.1,
+			"C_HI" : .4,
+			"M_LO" : .04,
+			"C_LO" : .04,
+			"M_HI" : .4
 			}
 
 		elif self.model_type == 'vdL_MC':
@@ -283,7 +242,12 @@ class Model:
 			"s" : 1,
 			"sigma" : 0, #strength of coral-herbivore feedback
 			"eta" : 0, #strength of algae-herbivore feedback
-			"alpha" : 0.5 #strength of algae-coral feedback 
+			"alpha" : 0.5, #strength of algae-coral feedback 
+			"P0" : 0.1,
+			"C_HI" : .4,
+			"M_LO" : .04,
+			"C_LO" : .04,
+			"M_HI" : .4
 			}
 
 		elif self.model_type == 'vdL_MP':
@@ -299,7 +263,12 @@ class Model:
 			"s" : 1,
 			"sigma" : 0, #strength of coral-herbivore feedback
 			"eta" : 2, #strength of algae-herbivore feedback
-			"alpha" : 0 #strength of algae-coral feedback 
+			"alpha" : 0, #strength of algae-coral feedback 
+			"P0" : 0.1,
+			"C_HI" : .4,
+			"M_LO" : .04,
+			"C_LO" : .04,
+			"M_HI" : .4
 			}
 
 		elif self.model_type == 'vdL_PC':
@@ -315,7 +284,12 @@ class Model:
 			"s" : 1,
 			"sigma" : .5, #strength of coral-herbivore feedback
 			"eta" : 0, #strength of algae-herbivore feedback
-			"alpha" : 0 #strength of algae-coral feedback 
+			"alpha" : 0, #strength of algae-coral feedback 
+			"P0" : 0.1,
+			"C_HI" : .4,
+			"M_LO" : .04,
+			"C_LO" : .04,
+			"M_HI" : .4
 			}
 
 		elif self.model_type == 'BM':
@@ -363,7 +337,12 @@ class Model:
 			"C_max" : .509, # coral cover with no fishing
 			"P_max" : 20, # parrotfish with no fishing
 			"M_max" : .466, # algal cover with really high fishing - note this is Mi only
-			"f_crit" : .275 # highest fishing at which coral wins (regardless of initial)
+			"f_crit" : .275, # highest fishing at which coral wins (regardless of initial)
+			"P0" : 0.1,
+			"C_HI" : .4,
+			"M_LO" : .04,
+			"C_LO" : .04,
+			"M_HI" : .4
 			}
 
 		for name, val in params.items():
@@ -372,23 +351,156 @@ class Model:
 			#exec(f"{name} = {val}", globals())
 			setattr(self, name, val)
 
+	# management parameter setter 
+	def set_mgmt_params(self, closure_length, f, m, poaching):
+		self.closure_length = closure_length
+		self.f = f
+		self.m = m
+		self.poaching = poaching 
+
 	# test accessor
 	def get_M(self):
 		return self.M
 
+
+
+# puttin patch system outside the class so we don't have to pass self as first parameter 
+# this way we can pass this function directly to odeint, and just call it from within the Model class 
+# don't necessarily need outside class, just can't modify object...
+# how could we bundle all of this into a library (ie import periodic_closures_modeltools) ? 
+
+def patch_system(X, t, system_model):
+		P_influx = [0]*system_model.n
+
+		for i in range(system_model.n):
+			for j in range(system_model.n):
+				P_influx[i] += (system_model.kP[i][j]) * system_model.P[j]  
+	
+			# better way to do this ?
+
+			if system_model.model_type == 'RB':
+				
+				return rass_briggs(X, i, system_model)
+			elif system_model.model_type == 'BM':
+				
+				return blackwood(X, i, system_model)
+			elif system_model.model_type == 'vdL_PC':
+				
+				return leemput(X, i, system_model)
+
+			elif system_model.model_type == 'vdL_MP':
+				
+				return leemput(X, i, system_model)
+				
+			elif system_model.model_type == 'vdL_MC':
+				
+				return leemput(X, i, system_model)
+				
+			elif system_model.model_type == 'vdL': # all feedbacks active 
+				
+				return leemput(X, t, i, system_model, P_influx)
+				
+			else:
+				print("Bad input, defaulting to Blackwood-Mumby!")
+				return blackwood(X, i, system_model)
+
+def rass_briggs(X, i, system_model):
+
+		P, C, Mv, Mi = X.reshape(4, n)
+		T = 1 - C - Mv - Mi 
+		dPdt = rH*P*(1-P/K) - system_model.fishing(P[i], f)*P[i] *(system_model.square_signal(t, closure_length, i, m, n, poaching))
+		dCdt = (phiC*T) + gTC*T*C - gamma*gTI*Mi*C - dC*C
+		dMvdt = phiM*T + rM*T*Mi + gTV*T*Mv - dV*Mv - P*Mv*Graze - omega * Mv
+		# conceptual question: why is that second term multiplied by M not Mv? 
+		dMidt = omega*Mv + gTI*T*Mi + gamma*gTI*Mi*C - dI*Mi
+		return [dPdt, dCdt, dMvdt, dMidt]
+
+		# check input
+		return None 
+
+def K(sigma, C):
+		return (1-sigma)+sigma*C
+
+def square_signal(t, closure_length, region, m, n, poaching):
+	start = int((t % (n*closure_length))/closure_length)
+	if start+m-1 >= n:
+		end = (start + m - 1)%n
+
+	else:
+		end = (start + m - 1)
+	if region >= start and region <= end:
+		return poaching
+	elif start + m - 1 >= n and (region >= start or region <= end):
+		return poaching
+	else:
+		#determine whether to bake displacement into signal
+		return (1-(m/n)*poaching)#/(1-(m/n))
+	
+#this signal function is not quite working yet 
+def sigmoid_signal(t, period, p):
+	if period == 0:
+		return 0
+	else:
+		return 1.0 / (1 + math.exp(-(t % period - p * period)))
+
+# idk
+def fishing(parrotfish, f):
+	steepness = 0.1
+	shift = -10
+	return f/(1+math.exp(-steepness*(parrotfish-shift)))
+
+# is self keyword necessary here ? or can we call these functions regardless?
+# is it necessary to send X as a parameter if it is embedded in the class? 
+# should all of these be class methods which can change self.X? 
+def blackwood(X, i, system_model):
+	P, C, M = X.reshape(3, n)
+
+	dPs[i] = s*P[i]*(1 - (P[i] / (beta*system_model.K(C[i])))) - system_model.fishing(P[i], f)*P[i]*system_model.square_signal(t, closure_length, i, m, n, poaching)
+	dCs[i] = r*(1-M[i]-C[i])*C[i]-d*C[i] - a*M[i]*C[i] + i_C*(1-M[i]-C[i])
+	# need to define g(P) before this model is used 
+	dMs[i] = a*M[i]*C[i] - g(P[i])*M[i] *(1/(1-C[i])) + gamma*M[i]*(1-M[i]-C[i])+i_M*(1-M[i]-C[i])
+
+	return np.concatenate((dPs, dCs, dMs), axis=0)
+
+# will need to pass [self.P, self.C, self.M] to this for it to work 
+def leemput(X, t, i, system_model, P_influx): # COPY THIS FORMAT FOR OTHER MODELS 
+	# check input 
+	P,C,M = X.reshape(3, system_model.n) # will reshaping work since we are passing arrays of length n? 
+	system_model.dPs[i] = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - fishing(P[i], system_model.f)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	system_model.dCs[i] = (system_model.i_C + system_model.r*C[i])*(1-M[i]-C[i])*(1-system_model.alpha*M[i]) - system_model.d*C[i]
+	
+	system_model.dMs[i] = (system_model.i_M+system_model.gamma*M[i])*(1-M[i]-C[i])-system_model.g*M[i]*P[i]/(system_model.g*system_model.eta*M[i]+1)
+
+	#concatenate into 1D vector to pass to next step
+	return np.concatenate((system_model.dPs, system_model.dCs, system_model.dMs), axis=0)
+
+
 def main():
-	x = Model('RB', 2, 1)
+
+	
+	yrs = 1000 #total amount of time
+	t = np.linspace(0, yrs, yrs) #timestep array -- same number of timesteps as years 
+	P0, C0L, C0H, M0L, M0H = 0.1, 0.04, 0.4, 0.04, 0.4
+
+	x = Model('vdL', 2, 1)
 	y = Model('vdL', 10, 0.97)
+
 	print(x.model_type)
 	print(y.model_type)
+
 	x.load_parameters()
 	y.load_parameters()
-	x.initialize_patch_model()
-	y.initialize_patch_model()
+
+	x.initialize_patch_model(P0, C0L, C0H, M0L, M0H)
+	y.initialize_patch_model(P0, C0L, C0H, M0L, M0H)
+
 	print(y.gamma)
 	print(y.get_M())
+	x.set_mgmt_params(20, 0.25, 1, 0)
+	print(x.run_model(x.X1, t)) # IT WORKED !!!!!!
 	# print(x.run_model(parameter list))
 	# x.time_series(parameter list, show = True)
+
 	return 
 
 if __name__ == '__main__':
