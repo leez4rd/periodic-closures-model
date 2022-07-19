349864d99b7fdb5aae6144c5836c381c4a37c923
Lee wall <lmw17@my.fsu.edu>
2022-07-19 17:42:50 -0400

    added binary search for unstable equilibrium

--- model.py
+++ model.py
@@ -133,13 +133,13 @@ class Model:
 		IC_set = self.X1 	# default to coral-rare 
 		MAX_TIME = len(t) # last year in the model run 
 
-		coral_array =  np.zeros(shape=(self.n,self.n)) # array of long-term coral averages
-		CL_array = np.empty(self.n) # array of closure lenghts 
-		m_array = np.empty(self.n)  # array of number of closures 
-		
-		# iterate over all scenarios 
-		for closure_length in range(1,self.n+1):
-			for m in range(self.n):
+		coral_array =  np.zeros(shape=(int(self.n), int(self.n))) # array of long-term coral averages
+		# CL_array = np.empty(int(0.75*self.n+1)) # array of closure lenghts 
+		# m_array = np.empty(int(self.n / 2))  # array of number of closures 
+		print(int(self.n / 2))
+		# iterate over  all scenarios 
+		for closure_length in range(1,int(self.n)):
+			for m in range(int(self.n)):
 				# set management parameters for this run 
 				self.set_mgmt_params(closure_length, fishing_intensity, m, self.poaching)
 
@@ -148,24 +148,30 @@ class Model:
 				# average over coral cover of last two full rotations for a single patch (assumes symmetry, may fix that)
 				avg = 0
 
-				for year in range(MAX_TIME - (MAX_TIME % (self.n*closure_length)) - 2*(self.n*closure_length), 
-				MAX_TIME - (MAX_TIME % (self.n*closure_length))):
+				# dealing with very large period / millennium ratio
+				
+				for year in range(MAX_TIME - (MAX_TIME % (self.n*closure_length)) - (self.n*closure_length), 
+					MAX_TIME - (MAX_TIME % (self.n*closure_length))):
 					avg += sol[year][self.n]
 				
-				avg = avg / (2*(self.n*closure_length) + 1)
-				print("Average coral cover for closure length: ", 3*closure_length, " and closure num: ", m)
+				avg = avg / ((self.n*closure_length) + 1)
+				print("Average coral cover for closure length: ", closure_length, " and closure num: ", m)
 				print(avg)
 				coral_array[closure_length-1][m] = avg
-				CL_array[closure_length-1] = closure_length
-				m_array[m] = m
+				# CL_array[closure_length-1] = closure_length -- don't think this is necessary 
+				# m_array[m] = m
 		plt.figure()
+		
+		# axes = plot.axes(projection='3d')
+		# axes.plot_surface(X1, Y1, Z1)
+		plot.show()
 		plt.title('Long-term outcomes for coral', fontsize = 20)
-		f = lambda y:y
+		f = lambda y:self.n*y
 		new_labels = [f(y) for y in range(1, self.n+1)]
-		ax = sb.heatmap(coral_array, vmin = 0.0, vmax = 1.0, cmap = "Spectral", yticklabels = new_labels, cbar = True) #YlGnBu for original color scheme
+		ax = sb.heatmap(coral_array, vmin = 0.0, vmax = 1.0, cmap = "mako", yticklabels = new_labels, cbar = True) #YlGnBu for original color scheme
 		ax.invert_yaxis()
-		plt.ylabel('Closure length in years', fontsize = 10) # x-axis label with fontsize 15
-		plt.xlabel('Number of patches closed', fontsize = 10) # y-axis label with fontsize 15
+		plt.ylabel('Period in years', fontsize = 10) # x-axis label with fontsize 15
+		plt.xlabel('Number of patches closed', fontsize = 10) # y-axis label with fontsize 15q
 		# ax.yticks(ax.get_yticks(), ax.get_yticks() * 3)
 		plt.yticks(rotation=0)
 		'''
@@ -188,12 +194,108 @@ class Model:
 
 		return None
 
-	def bistable_zone():
+	def bistable_zone(self, t):
+		""" plot final coral cover for different values of fishing effort for two sets of initial conditions """ 
+		final_coral_high = np.empty(50)
+		final_coral_low = np.empty(50)
 
-		return None
-	def find_unstable_equilibrium():
-		return None
-	def scenario_plot():
+		fishing_range = np.linspace(0, 0.99, 50)
+
+		for f in fishing_range:
+
+			# set management parameters 
+			self.set_mgmt_params(0, f, 0, self.poaching)
+
+			# make high start solution
+			high_sol = odeint(patch_system, self.X2, t, args = (self, ))
+
+			# make low start solution 
+			low_sol = odeint(patch_system, self.X1, t, args = (self, ))
+
+			# note: this only works without periodic oscillations, which this plot assumes are not present 
+			yrs = len(t)
+			print(f*100)
+			final_coral_high[int(f * 50)] = high_sol[yrs - 1][1]
+
+			final_coral_low[int(f * 50)] = low_sol[yrs - 1][1]
+
+		plt.plot(fishing_range, final_coral_low, label = 'coral starts low', color = 'blue')
+		plt.plot(fishing_range, final_coral_high, label = 'coral starts high' , color = 'green')
+		plt.show()
+
+
+	def find_unstable_equilibrium(self, t, lowC = 0.1, highC = 0.7, recursion_depth = 0):
+		""" binary search for unstable equilibrium """ 
+
+
+		midpoint = (lowC + highC) / 2
+		print(midpoint)
+
+
+		IC_mid = [0.1]*self.n + [midpoint]*self.n + [0.04]*self.n # verify this 
+
+		# run model starting from midpoint -- could reduce runtime by making t smaller 
+		mid_sol = odeint(patch_system, IC_mid, t, args = (self, ))
+
+		if recursion_depth > 10:
+			print("Close enough....")
+			return midpoint
+
+		# if coral cover grows from the midpoint, the equilibrium is above it
+		if mid_sol[len(t) - 1][1] - midpoint > 0:
+			print("going up...")
+			new_recursion_depth = recursion_depth + 1
+			return self.find_unstable_equilibrium(t, lowC = midpoint, recursion_depth = new_recursion_depth)
+		# if coral cover declines from the midpoint, the equilibrium is below it 
+		elif mid_sol[len(t) - 1][1] - midpoint < 0: 
+			print("going down...")
+			new_recursion_depth = recursion_depth + 1
+			return self.find_unstable_equilibrium(t, highC = midpoint, recursion_depth = new_recursion_depth)
+		else: # unstable equilibrium found!
+			return midpoint 
+
+	def scenario_plot(self, t, fishing_intensity, IC_set):
+
+		final_coral = np.empty(self.n)
+		ms = np.empty(self.n)
+		periods = [5, 20, 50, 100, 200]  # parametrize in terms of coral growth time? 
+		# there is a cooler way to do colors than this 
+		color_sequence = {5: '#1f77b4', 20: '#aec7e8', 50: '#ff7f0e', 100:'#ffbb78', 200:'#2ca02c'}
+
+		MAX_TIME = len(t)
+		
+
+		for period in periods:
+			print(period)
+			for m in range(self.n):
+				print(m)
+
+				# set management parameters for this run 
+				self.set_mgmt_params(period / self.n, fishing_intensity, m, self.poaching)
+
+				# solve ODE system 
+				sol = odeint(patch_system, IC_set, t, args = (self, ))
+
+				avg = 0
+
+				# truncate to last full period, then average over that period 
+				for year in range(MAX_TIME - MAX_TIME % period - period, MAX_TIME - MAX_TIME % period):
+					avg += sol[year][self.n] # only looking at one patch here
+					# may amend to average over all patches for comparison with MPA 
+				
+				avg = avg / (period + 1)
+				final_coral[m] = avg
+				print(avg)
+				ms[m] = m
+
+			# plot result for this period
+			
+			plt.plot(ms, final_coral, label = 'period = %d' % period, color = color_sequence[period])
+		plt.xlabel('Number of closures')
+		plt.ylabel('Coral Cover')
+		plt.title('Final coral state across closure scenarios')
+		plt.legend(loc=0)
+		plt.show()
 
 		return None 
 
@@ -397,24 +499,39 @@ def patch_system(X, t, system_model):
 			return np.concatenate((system_model.dPs, system_model.dCs, system_model.dMs), axis = 0)
 
 def rass_briggs(X, t, i, system_model, P_influx):
+	P,C,M = X.reshape(4, system_model.n) # will reshaping work since we are passing arrays of length n? 
+	T = 1 - C[i] - Mv[i] - Mi[i]
+	# dC = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - fishing(P[i], system_model.f)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	dP = P_influx[i]+ system_model.rH*P[i]*(1-P[i]/system_model.K) - system_model.f/(1-system_model.m/system_model.n)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	# print(P_influx)
+	# print(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	dC = (system_model.phiC*T) + system_model.gTC*T*C[i] - system_model.gamma*system_model.gTI*Mi[i]*C[i] - system_model.dC*C[i]
+	dMv = system_model.phiM*T + system_model.rM*T*Mi[i] + system_model.gTV*T*Mv[i] - system_model.dV*Mv[i] - P[i]*Mv[i]*system_model.Graze - system_model.omega * Mv[i]
+	dMi = system_model.omega*Mv[i] + system_model.gTI*T*Mi[i] + system_model.gamma*system_model.gTI*Mi[i]*C[i] - system_model.dI*Mi[i]
+	return [dP, dC, dMv, dMi]
+	'''
 
-		P, C, Mv, Mi = X.reshape(4, n)
-		T = 1 - C - Mv - Mi 
-		dPdt = rH*P*(1-P/K) - system_model.fishing(P[i], f)*P[i] *(system_model.square_signal(t, closure_length, i, m, n, poaching))
-		dCdt = (phiC*T) + gTC*T*C - gamma*gTI*Mi*C - dC*C
-		dMvdt = phiM*T + rM*T*Mi + gTV*T*Mv - dV*Mv - P*Mv*Graze - omega * Mv
-		# conceptual question: why is that second term multiplied by M not Mv? 
-		dMidt = omega*Mv + gTI*T*Mi + gamma*gTI*Mi*C - dI*Mi
-		return [dPdt, dCdt, dMvdt, dMidt]
+	P, C, Mv, Mi = X.reshape(4, n)
+	T = 1 - C - Mv - Mi 
+	dPdt = rH*P*(1-P/K) - system_model.fishing(P[i], f)*P[i] *(system_model.square_signal(t, closure_length, i, m, n, poaching))
+	dCdt = (phiC*T) + gTC*T*C - gamma*gTI*Mi*C - dC*C
+	dMvdt = phiM*T + rM*T*Mi + gTV*T*Mv - dV*Mv - P*Mv*Graze - omega * Mv
+	# conceptual question: why is that second term multiplied by M not Mv? 
+	dMidt = omega*Mv + gTI*T*Mi + gamma*gTI*Mi*C - dI*Mi
+	return [dPdt, dCdt, dMvdt, dMidt]
+	'''
 
-		# check input
-		return None 
+	# check input
+	# return None 
 
 def K(sigma, C):
 		return (1-sigma)+sigma*C
 
 def square_signal(t, closure_length, region, m, n, poaching):
-	start = int((t % (n*closure_length))/closure_length)
+	if closure_length != 0: 
+		start = int((t % (n*closure_length))/closure_length)
+	else:
+		start = 0
 	if start+m-1 >= n:
 		end = (start + m - 1)%n
 
@@ -445,6 +562,7 @@ def fishing(parrotfish, f):
 # is it necessary to send X as a parameter if it is embedded in the class? 
 # should all of these be class methods which can change self.X? 
 def blackwood(X, t, i, system_model, P_influx):
+	'''
 	P, C, M = X.reshape(3, n)
 
 	# dPs[i] = s*P[i]*(1 - (P[i] / (beta*system_model.K(C[i])))) - system_model.fishing(P[i], f)*P[i]*system_model.square_signal(t, closure_length, i, m, n, poaching)
@@ -454,22 +572,35 @@ def blackwood(X, t, i, system_model, P_influx):
 	dMs[i] = a*M[i]*C[i] - g(P[i])*M[i] *(1/(1-C[i])) + gamma*M[i]*(1-M[i]-C[i])+i_M*(1-M[i]-C[i])
 
 	return np.concatenate((dPs, dCs, dMs), axis=0)
+	'''
+	P,C,M = X.reshape(3, system_model.n) # will reshaping work since we are passing arrays of length n? 
+	# dC = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - fishing(P[i], system_model.f)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	dP = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - system_model.f/(1-system_model.m/system_model.n)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	# need separtate K function for blackwood
+	# print(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	dC = (system_model.r*C[i])*(1-M[i]-C[i]) - system_model.a*M[i]*C[i] - system_model.d*C[i] # recruitment needed?
+	
+	dM = (system_model.gamma*M[i])*(1-M[i]-C[i])-system_model.g(P[i])*(1/(1-C[i]))*M[i]
+
+
+	return [dP, dC, dM]
 
 # will need to pass [self.P, self.C, self.M] to this for it to work 
 def leemput(X, t, i, system_model, P_influx): # COPY THIS FORMAT FOR OTHER MODELS 
 	# check input 
 	P,C,M = X.reshape(3, system_model.n) # will reshaping work since we are passing arrays of length n? 
-	# blep = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - fishing(P[i], system_model.f)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
-	blep = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - system_model.f/(1-system_model.m/system_model.n)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	# dC = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - fishing(P[i], system_model.f)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
+	dP = P_influx[i]+ system_model.s*P[i]*(1 - (P[i] / K(system_model.sigma,C[i]))) - system_model.f/(1-system_model.m/system_model.n)*P[i] *(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
 	# print(P_influx)
 	# print(square_signal(t, system_model.closure_length, i, system_model.m, system_model.n, system_model.poaching))
-	blop = (system_model.i_C + system_model.r*C[i])*(1-M[i]-C[i])*(1-system_model.alpha*M[i]) - system_model.d*C[i]
+	dC = (system_model.i_C + system_model.r*C[i])*(1-M[i]-C[i])*(1-system_model.alpha*M[i]) - system_model.d*C[i]
 	
-	bleep = (system_model.i_M+system_model.gamma*M[i])*(1-M[i]-C[i])-system_model.g*M[i]*P[i]/(system_model.g*system_model.eta*M[i]+1)
-	return [blep, blop, bleep]
+	dM = (system_model.i_M+system_model.gamma*M[i])*(1-M[i]-C[i]) - system_model.g*M[i]*P[i]/(system_model.g*system_model.eta*M[i]+1)
+
+	return [dP, dC, dM]
 	# NEED TO CHANGE OTHER FUNCTIONS TO MATCH BLEP BLOP BLEEP METHOD
 	# concatenate into 1D vector to pass to next step
-	# return np.concatenate((blep, blop, bleep), axis=0)
+	# return np.concatenate((dC, dC, dM), axis=0)
 
 
 def main():
@@ -481,7 +612,7 @@ def main():
 
 	# create Model objects
 	x = Model('vdL', 2, 1)
-	y = Model('vdL',5,  1) # ISSUE WITH DISPERSAL: kP calculation or P_influx must be incorrect 
+	y = Model('vdL', 10,  1) # ISSUE WITH DISPERSAL: kP calculation or P_influx must be incorrect 
 	z = Model('vdL', 20, 1)
 	
 	# load Model parameters according to model type
@@ -493,11 +624,15 @@ def main():
 	y.initialize_patch_model(P0, C0L, C0H, M0L, M0H)
 	z.initialize_patch_model(P0, C0L, C0H, M0L, M0H)
 	z.load_parameters() # do this inside initializer
-	# y.set_mgmt_params(20, 0.24, 1, 0)
+	y.set_mgmt_params(20, 0.24, 1, 0)
 	# y.time_series(y.X1, t, False, True)
 	# x.set_mgmt_params(20, 0.1, 1, 0) # set management parameters -- closure length, fishing effort, # of closures, poaching 
 	# print(x.run_model(x.X1, t)) # IT WORKED !!!!!!
-	# x.time_series(x.X1, t, False, True) 
+
+	x = y.find_unstable_equilibrium(t)
+	print(x)
+	# y.bistable_zone(t)
+	y.time_series(y.X1, t, save = False, show = True) 
 
 	# x.set_mgmt_params(40, 0.4, 1, 0.5)
 	# x.time_series(x.X1, t, False, True)
@@ -511,6 +646,10 @@ def main():
 	y.coral_recovery_map(t, 0.25)
 	y.coral_recovery_map(t, 0.30)
 	'''
+	ICs = y.X1 
+	y.scenario_plot(t, 0.3, ICs)
+
+
 	y.coral_recovery_map(t, 0.25)
 	y.coral_recovery_map(t, 0.35)
 	z.coral_recovery_map(t, 0.25)
